df1397a4031143eecabaf61c9f1ab82ca66a2f99
Thomas Mino Pradel <tmino@imagen.cl>
2021-10-09 23:10:52 -0300

    a

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ class App extends Component {
   state = {
     nombre: '',
     ventanasArr: [
-      { id: 0, titulo: 'Ventana0', isOpen: false, alto: 600, ancho: 400, footer: 'Thomas Enrique', atencion: false },
-      { id: 1, titulo: 'Ventana1', isOpen: false, alto: 600, ancho: 400, footer: 'Thomas Enrique', atencion: false },
-      { id: 2, titulo: 'Ventana2', isOpen: true, alto: 600, ancho: 400, footer: 'Thomas Enrique', atencion: false },
-      { id: 3, titulo: 'Ventana3', isOpen: true, alto: 600, ancho: 400, footer: 'Thomas Enrique', atencion: false },
-      { id: 4, titulo: 'Ventana3', isOpen: true, alto: 600, ancho: 400, footer: 'Thomas Enrique', atencion: false },
-      { id: 5, titulo: 'Ventana3', isOpen: true, alto: 600, ancho: 400, footer: 'Thomas Enrique', atencion: false },
-      { id: 6, titulo: 'Ventana3', isOpen: true, alto: 600, ancho: 400, footer: 'Thomas Enrique', atencion: false },
-      { id: 7, titulo: 'Ventana3', isOpen: true, alto: 600, ancho: 400, footer: 'Thomas Enrique', atencion: false },
+      { id: 0, titulo: 'Inicio', isOpen: false, alto: 700, ancho: 550, footer: 'Seleccione aplicación', atencion: false },
+      { id: 1, titulo: 'Ventana1', isOpen: false, alto: 700, ancho: 546, footer: 'Thomas Enrique', atencion: false },
+      { id: 2, titulo: 'Ventana2', isOpen: true, alto: 543, ancho: 687, footer: 'Thomas Enrique', atencion: false },
+      { id: 3, titulo: 'Ventana3', isOpen: true, alto: 745, ancho: 576, footer: 'Thomas Enrique', atencion: false },
+      { id: 4, titulo: 'Ventana3', isOpen: true, alto: 246, ancho: 890, footer: 'Thomas Enrique', atencion: false },
+      { id: 5, titulo: 'Ventana3', isOpen: true, alto: 345, ancho: 687, footer: 'Thomas Enrique', atencion: false },
+      { id: 6, titulo: 'Ventana3', isOpen: true, alto: 213, ancho: 465, footer: 'Thomas Enrique', atencion: false },
+      { id: 7, titulo: 'Ventana3', isOpen: true, alto: 746, ancho: 674, footer: 'Thomas Enrique', atencion: false },
     ],
     ProgramaArr: [
       { key: 1, text: "Programa n1", img: "img not found" },
@@ -67,11 +67,12 @@ class App extends Component {
       )
     }
 
-    const Ventana = ({ children, footer, titulo, isOpen, id }) => {
+    const Ventana = ({ children, footer, titulo, isOpen, alto, ancho, id }) => {
       let a = window.Event.y;
       if (isOpen) {
         return (
-          <div className="DivBody">
+
+          <div className={id == 0 ? "Inicio" : "DivBody"} style={{ width: ancho, height: alto }}>
             <div className="DivHeader">
               <div className="TituloHead">
                 <h3>{titulo}</h3>
@@ -112,7 +113,8 @@ class App extends Component {
                   titulo={v.titulo}
                   isOpen={v.isOpen}
                   footer={v.footer}
-
+                  alto={v.alto}
+                  ancho={v.ancho}
                 >
                   <div>
                     {/* <h1>Thomas Miño Pradel</h1> */}
@@ -124,9 +126,9 @@ class App extends Component {
               </>
             )
           })}
-
-
+          <div style={{ height: "45px", width: "100%" }}></div>
         </div>
+
         <BarraTarea
           VentanaP={this.state.ventanasArr}
           Programas={this.state.ProgramaArr}
